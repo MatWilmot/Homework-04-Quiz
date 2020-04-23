6529978ac2ec5282a79a0a6b98d7d5dcd601a1a4
Mat Wilmot <matwilmot90@gmail.com>
2020-04-23 14:32:47 -0700

    removed +1 btn, added conditional to create header and buttons

--- app.js
+++ app.js
@@ -1,33 +1,34 @@
 $(document).ready(function () {
   // create an array of objects for questions and answers
   var questions = [
-    ["Question 1", "Q1 A1", "Q1 A2", "Q1 A3", "Q1 A4"],
+    ["What color is the sky?", "red", "blue", "green", "yellow"],
     ["Question 2", "Q2 A1", "Q2 A2", "Q2 A3", "Q2 A4"],
   ];
 
   // create an empty variable for the current score, and prove I can increase it
   var score = 0;
-
-  $("#plusOne").on("click", function () {
-    score++;
-    console.log(score);
-  });
-  var questionIndex;
+  var questionIndex = 0;
 
   // create a function to refresh the html on the screen each time the question is changed
   function refreshList() {
     // clear out the main area
     $("#questions").html("");
-    questionIndex = 0;
     // prepend each item in the object to the screen
     for (var i = 0; i < Object.keys(questions[questionIndex]).length; i++) {
-      $("#questions").append(`<div class="card mb-3">
-      <div class="card-body">
-        <button class="card-text">
-          ${JSON.stringify(questions[questionIndex][i])}
-        </button>
-      </div>
-    </div>`);
+      if (i === 0) {
+        $("#header").append(`
+          <h1 data-id=${i}>
+            ${questions[questionIndex][i]}
+          </h1>`);
+      } else {
+        $("#questions").append(`<div class="card mb-3">
+        <div class="card-body">
+          <button data-id=${i} class="card-text answerBtn" style="width: 100px;">
+            ${questions[questionIndex][i]}
+          </button>
+        </div>
+      </div>`);
+      }
     }
   }
   refreshList();
@@ -35,7 +36,7 @@ $(document).ready(function () {
 
 // add a timer
 var timerEl = document.querySelector("#timerEl");
-var secondsLeft = 10;
+var secondsLeft = 30;
 
 function setSpeed() {
   var timerInterval = setInterval(function () {
@@ -55,34 +56,3 @@ function sendMessage() {
   $("#header").html("<h1>Time's Up!</h1>");
   $("#questions").html("<button>View Score</button>");
 }
-
-// // print a question and its answers to the screen
-// var questionIndex = 0;
-// // button elements are persistent as long as user is taking quiz
-// var header = document.createElement("h1");
-// var answer1 = document.createElement("button");
-// answer1.setAttribute("id", questions[questionIndex].wrong1);
-// answer1.setAttribute("class", "answerBtn");
-// // var answer2 = document.createElement("button");
-// // var answer3 = document.createElement("button");
-// // var answer4 = document.createElement("button");
-
-// // content of each button should change based on the question
-// header.innerHTML = questions[0].question;
-// document.getElementById("header").appendChild(header);
-
-// answer1.innerHTML = questions[0].answer1;
-// document.getElementById("questions").appendChild(answer1);
-
-// document.querySelector(".answerBtn").addEventListener("click", function () {
-//   questionIndex++;
-//   var answerBtnId = document.querySelector(this).getAttribute("id");
-//   if ((answerBtnId = "correct")) {
-//     score++;
-//   } else {
-//     console.log("wrong!");
-//   }
-//   console.log(answerBtnId);
-
-//   // console.log(questionIndex);
-// });
